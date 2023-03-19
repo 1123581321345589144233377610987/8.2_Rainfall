071539050fc51c7268044ec4c070f406509689c4
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2023-03-18 22:54:32 -0500

    Update 8.0.2_Rainfall.py

--- 8.0.2_Rainfall.py
+++ 8.0.2_Rainfall.py
@@ -4,13 +4,21 @@ data.close()
 total = 0
 count = 0
 for i in Data:
-    i = i.strip("/n")
+    i = i.strip("\n")
     I = i.split(" ")
+#FIXED WAY
+    II = I[1].split('.')
+    if II[0].isdigit() and II[1].isdigit():
+        total += float(I[1])
+        count += 1
+    else:
+        print(f"Bad numeric data found for entry: {i}")
+'''MISUNDERSTOOD DIRECTIONS:
     try:
         total += float(I[1])
         count += 1
     except ValueError:
-        print(f"Bad numeric data found for entry: {i}")
+        print(f"Bad numeric data found for entry: {i}")'''
 print(f"{count} good values were found.")
 print(f"The total was {total:.1f}.")
 print(f"The average was {total/count:.1f}.")
